d9bcf60f9d6a81fc640fff983161b81697700587
Ann-Anna <annaciata23@gmail.com>
2026-05-19 11:21:40 +0300

    great learning

---
+++ WinFormsApp1/Form1.Designer.vb
@@ -0,0 +1,130 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Button1 = New Button()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(173, 47)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(59, 25)
+        Label1.TabIndex = 0
+        Label1.Text = "Name"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(183, 136)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(97, 25)
+        Label2.TabIndex = 1
+        Label2.Text = "Nationality"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(183, 214)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(44, 25)
+        Label3.TabIndex = 2
+        Label3.Text = "Age"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(312, 343)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(80, 25)
+        Label4.TabIndex = 3
+        Label4.Text = "RESULTS"
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(346, 282)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(193, 34)
+        Button1.TabIndex = 4
+        Button1.Text = "Check Eligibility"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(308, 44)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(150, 31)
+        TextBox1.TabIndex = 5
+        TextBox1.Text = "b"
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(308, 133)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(150, 31)
+        TextBox2.TabIndex = 6
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(312, 208)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(150, 31)
+        TextBox3.TabIndex = 7
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Button1)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+
+End Class
---
+++ WinFormsApp1/Form1.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim name As String
+        Dim nationality As String
+        Dim age As Integer
+
+        name = TextBox1.Text
+        nationality = TextBox2.Text
+        age = Val(TextBox3.Text)
+
+        If nationality = "Kenyan" And age >= 18 Then
+            Label4.Text = name & " is eligible to vote."
+        ElseIf nationality <>"Kenyan" And age >= 18 Then
+            Label4.Text = name & " is not eligible to vote because they are not Kenyan."
+        ElseIf nationality = "Kenyan" And age < 18 Then
+            Label4.Text = name & " is not eligible to vote because they are below 18."
+        Else
+            Label4.Text = name & " is not eligible to vote."
+        End If
+    End Sub
+End Class
